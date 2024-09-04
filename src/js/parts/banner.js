cfad1e59ac0e55236756ea7300e9d44e36c446e3
Uzurch1k <galcenkovlad7@gmail.com>
2024-09-04 22:50:09 +0300

    fix

--- src/js/parts/banner.js
+++ src/js/parts/banner.js
@@ -1,32 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   const bannerCarousell = document.querySelector('.banner__carousell');
 
-  const bannerList = document.querySelector('.banner__list');
   const carousellPartF = document.querySelector('.carousell-part1');
   const carousellPartS = document.querySelector('.carousell-part2');
 
   function updateMarqueeAnimation() {
     const listWidthPartF = carousellPartF.scrollWidth; // Полная ширина списка
     const listWidthPartS = carousellPartS.scrollWidth; // Полная ширина списка
-    console.log(listWidthPartF);
-    console.log(listWidthPartS);
 
     const carousellWidth = bannerCarousell.clientWidth; // Ширина видимой области
-    console.log(carousellWidth);
-
-    // const spacePartS = listWidthPartS - carousellWidth;
 
     // const duration = (listWidth + extraSpace) / 200;
-
     // bannerList.style.animationDuration = `${duration}s`;
 
     const moveValueF = listWidthPartF - carousellWidth;
-    carousellPartF.style.setProperty('--move1', `${Math.abs(moveValueF)}px`);
-    console.log(moveValueF);
+    carousellPartF.style.setProperty('--move1-p', `${Math.abs(moveValueF)}px`);
+    carousellPartF.style.setProperty('--move1-m', `-${Math.abs(moveValueF)}px`);
 
     const moveValueS = listWidthPartS - carousellWidth;
-    carousellPartS.style.setProperty('--move2', `${Math.abs(moveValueS)}px`);
-    console.log(moveValueS);
+    carousellPartS.style.setProperty('--move2-p', `${Math.abs(moveValueS)}px`);
+    carousellPartS.style.setProperty('--move2-m', `-${Math.abs(moveValueS)}px`);
   }
 
   updateMarqueeAnimation();
