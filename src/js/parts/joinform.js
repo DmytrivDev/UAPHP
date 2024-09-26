dd2e94748c50a64ef6d0019cf57577c86fd36a1a
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-09-26 18:22:03 +0300

    Fixes

--- src/js/parts/joinform.js
+++ src/js/parts/joinform.js
@@ -2,59 +2,10 @@ const openPopup = document.querySelectorAll('.open-popup');
 
 const joinForm = document.querySelector('.joinform');
 const closeBtn = document.querySelector('.joinform__close');
-const sendBtn = document.querySelector('.joinform__send');
 const sendDone = document.querySelector('.joinform__done');
-
-const form = document.querySelector('.joinform__form');
-const part1 = document.querySelector('.joinform__part1');
-const part2 = document.querySelector('.joinform__part2');
 
 const body = document.body;
 const header = document.querySelector('.header');
-
-// Валідація форми
-function validateForm() {
-  let isValid = true;
-
-  const nameField = form.querySelector('input[name="join-name"]');
-  const emailField = form.querySelector('input[name="join-email"]');
-  const nameLabel = nameField.closest('.joinform__label');
-  const emailLabel = emailField.closest('.joinform__label');
-
-  nameLabel.classList.remove('is-error');
-  emailLabel.classList.remove('is-error');
-  sendBtn.classList.remove('disabled');
-
-  if (nameField.value.trim() === '') {
-    nameLabel.classList.add('is-error');
-    sendBtn.classList.add('disabled');
-    isValid = false;
-  }
-
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(emailField.value.trim())) {
-    emailLabel.classList.add('is-error');
-    sendBtn.classList.add('disabled');
-    isValid = false;
-  }
-
-  sendBtn.disabled = !isValid;
-
-  return isValid;
-}
-
-// Відправка форми в консоль
-function submitForm() {
-  const formData = {
-    name: form.querySelector('input[name="join-name"]').value,
-    email: form.querySelector('input[name="join-email"]').value,
-    comment: form.querySelector('textarea[name="join-mess"]').value,
-  };
-
-  console.log('Form submitted: ', formData);
-
-  form.reset();
-}
 
 // Функція для відкриття модального вікна
 function openModal(evt) {
@@ -100,28 +51,16 @@ function closeModal() {
 }
 
 // Перехід до "Успішно!" сторінки
-function showSuccess() {
-  part1.classList.remove('is-transition', 'is-step');
-  part2.classList.add('is-transition', 'is-step');
+export function showSuccess() {
+  const part1N = document.querySelector('.joinform__part1');
+  const part2N = document.querySelector('.joinform__part2');
+
+  part1N.classList.remove('is-transition', 'is-step');
+  part2N.classList.add('is-transition', 'is-step');
 }
 
 // Обробка натискання кнопки "Подати заявку"
-sendBtn?.addEventListener('click', function (event) {
-  event.preventDefault();
-
-  if (validateForm()) {
-    showSuccess();
-    submitForm();
-  }
-});
-
-// Перевірка полів на введення даних у реальному часі
-const nameField = form?.querySelector('input[name="join-name"]');
-const emailField = form?.querySelector('input[name="join-email"]');
-
-// Обробники подій для валідації під час введення
-nameField?.addEventListener('input', validateForm);
-emailField?.addEventListener('input', validateForm);
+// showSuccess();
 
 // Відкриття модального вікна при натисканні на кнопку "Стати учасником"
 openPopup?.forEach(e => {
